65794c59c93bdecaf21ae1fba6e92be9ed89fda5
faye <faye@faye.com>
2019-08-28 17:15:39 +0800

    master branch@T430 190828 add faye_subDevice

--- device_test/device_test.c
+++ device_test/device_test.c
@@ -19,33 +19,44 @@ char dev_attr2_Buf[ PAGE_SIZE+1 ] = "device attribute 2 data";
 
 char dev_attrGroup_Buf[ PAGE_SIZE+1 ] = "device attribute group data";
 
+char subDev_attr_Buf[ PAGE_SIZE+1 ] = "sudDevice attribute data";
+
 void  faye_device_release( struct device *dev ){
-	printk( "### in faye_device_release() start ###\n" );
-	printk(  "device is:%s\n", dev_name(dev) );
-	printk( "### in faye_device_release() end ###\n" );
+    printk( "### in faye_device_release() start ###\n" );
+    printk(  "device is:%s\n", dev_name(dev) );
+    printk( "### in faye_device_release() end ###\n" );
+}
+
+void faye_subDevice_release( struct device *dev ){
+    printk( "### in faye_subDevice_release() start ###\n" );
+    printk( "device is:%s\n", dev_name(dev) );
+    printk( "### in faye_subDevice_release() end ###\n" );
 }
 
 /* 定义一个device_attribute对象,将其封装到attribute_group对象中 */
 static ssize_t device_attribute_group_show( struct device *dev, struct device_attribute *attr, char *buf ){
-	return snprintf( buf, PAGE_SIZE, "%s\n", dev_attrGroup_Buf );
+    return snprintf( buf, PAGE_SIZE, "%s\n", dev_attrGroup_Buf );
 }
 static ssize_t device_attribute_group_store( struct device *dev, struct device_attribute *attr, const char *buf, size_t count ){
-	return sprintf( dev_attrGroup_Buf, "%s", buf );
+    return sprintf( dev_attrGroup_Buf, "%s", buf );
 }
 static DEVICE_ATTR( faye_device_attribute_group, (S_IRUGO|S_IWUSR|S_IWGRP), device_attribute_group_show, device_attribute_group_store );
 
 struct attribute_group faye_device_attrGroup = {
-	.name  = "faye_device_attrGroup_name",
-	.attrs = (struct attribute*[]){ &(dev_attr_faye_device_attribute_group.attr), NULL },
+    .name  = "faye_device_attrGroup_name",
+    .attrs = (struct attribute*[]){ &(dev_attr_faye_device_attribute_group.attr), NULL },
 };
 
 struct device faye_device = {
-	.init_name	= "faye_device",
-	.bus 		= &faye_bus,
-	//.parent		= &faye_busDevice,
-	.release	=  faye_device_release,
-	.groups     = ( const struct attribute_group*[] ){ &faye_device_attrGroup, NULL },
-	//.class		= &faye_class,
+    .init_name  = "faye_device",
+    .bus        = &faye_bus,
+    //.parent       = &faye_busDevice,
+            /* 若未设置.parent字段，但其所属总线的bus.dev_root字段被赋值，
+             * 则本device对象会成为dev_root的子设备
+             * 可参考本文中的“bus_type数据结构：”部分 */
+    .release    =  faye_device_release,
+    .groups     = ( const struct attribute_group*[] ){ &faye_device_attrGroup, NULL },
+    //.class        = &faye_class,
 };
 
 struct device faye_device_class = {
@@ -53,69 +64,96 @@ struct device faye_device_class = {
     .parent    = &faye_busDevice,
     .release   = &faye_class_dev_release, 
     .groups    = ( const struct attribute_group*[] ){ &faye_device_attrGroup, NULL },
-	.class     = &faye_class,
+    .class     = &faye_class,
 };
 
 static ssize_t faye_device_attr1_show( struct device *dev, struct device_attribute *attr, char *buf ){
-	return snprintf( buf, PAGE_SIZE, "%s\n", dev_attr1_Buf );
+    return snprintf( buf, PAGE_SIZE, "%s\n", dev_attr1_Buf );
 }
 static ssize_t faye_device_attr1_store( struct device *dev, struct device_attribute *attr, const char *buf, size_t count ){
-	return sprintf( dev_attr1_Buf, "%s", buf );	
+    return sprintf( dev_attr1_Buf, "%s", buf ); 
 }
 static DEVICE_ATTR( faye_device_attr1, (S_IRUGO|S_IWUSR|S_IWGRP), faye_device_attr1_show, faye_device_attr1_store );
 
 static ssize_t faye_device_attr2_show( struct device *dev, struct device_attribute *attr, char *buf ){
-	return snprintf( buf, PAGE_SIZE, "%s\n", dev_attr2_Buf );
+    return snprintf( buf, PAGE_SIZE, "%s\n", dev_attr2_Buf );
 }
 static ssize_t faye_device_attr2_store( struct device *dev, struct device_attribute *attr, const char *buf, size_t count ){
-	return sprintf( dev_attr2_Buf, "%s", buf );	
+    return sprintf( dev_attr2_Buf, "%s", buf ); 
 }
 static DEVICE_ATTR( faye_device_attr2, (S_IRUGO|S_IWUSR|S_IWGRP), faye_device_attr2_show, faye_device_attr2_store );
 
+struct device faye_subDevice = {
+    .init_name  = "faye_subDevice",
+    .bus        = &faye_bus,
+    .parent     = &faye_device,
+    .release    =  faye_subDevice_release,
+};
+
+static ssize_t faye_subDevice_attr_show( struct device *dev, struct device_attribute *attr, char *buf ){
+    return snprintf( buf, PAGE_SIZE, "%s\n", subDev_attr_Buf );   
+}
+static ssize_t faye_subDevice_attr_store( struct device *dev, struct device_attribute *attr, const char *buf, size_t count ){
+    return sprintf( subDev_attr_Buf, "%s", buf );    
+}
+static DEVICE_ATTR( faye_subDevice_attr, (S_IRUGO|S_IWUSR|S_IWGRP), faye_subDevice_attr_show, faye_subDevice_attr_store );
+
 static int __init faye_device_init( void ){
-	int ret;
+    int ret;
 
     printk( "/**** faye_device_init() start ***************************************/\n" );
 
-	ret = device_register( &faye_device );
-	if( ret ){
-		printk( KERN_DEBUG "Unable to register faye_device\n" );
-		return ret;
-	}
+    ret = device_register( &faye_device );
+    if( ret ){
+        printk( KERN_DEBUG "Unable to register faye_device\n" );
+        return ret;
+    }
 
-	if( device_create_file( &faye_device, &dev_attr_faye_device_attr1 ) ){
-		printk( KERN_DEBUG "Unable to create device attribute file\n" );
-		return ret;
-	}
-	
-	if( device_create_file( &faye_device, &dev_attr_faye_device_attr2 ) ){
-		printk( KERN_DEBUG "Unable to create device attribute file\n" );
-		return ret;
-	}
+    if( device_create_file( &faye_device, &dev_attr_faye_device_attr1 ) ){
+        printk( KERN_DEBUG "Unable to create device attribute file\n" );
+        return ret;
+    }
+    
+    if( device_create_file( &faye_device, &dev_attr_faye_device_attr2 ) ){
+        printk( KERN_DEBUG "Unable to create device attribute file\n" );
+        return ret;
+    }
 
-	printk( KERN_DEBUG "faye_device register success\n" );
-	
-	ret = device_register( &faye_device_class );
-	if( ret ){
-		printk( KERN_DEBUG "Unable to register faye_device_class\n" );
-		return ret;
-	}
-	printk( KERN_DEBUG "faye_device_class register success\n" );
-	
-	printk( "/**** faye_device_init end ***************************************/\n" );
-	return 0;
+    printk( KERN_DEBUG "faye_device register success\n" );
+    
+    ret = device_register( &faye_device_class );
+    if( ret ){
+        printk( KERN_DEBUG "Unable to register faye_device_class\n" );
+        return ret;
+    }
+    printk( KERN_DEBUG "faye_device_class register success\n" );
+
+    ret = device_register( &faye_subDevice );
+    if( ret ){
+        printk( KERN_DEBUG "unable to register faye_subDevice\n" );
+        return ret;
+    }
+    if( device_create_file( &faye_subDevice, &dev_attr_faye_subDevice_attr ) ){
+        printk( KERN_DEBUG "unable to create device attribute file\n" );
+        return ret;
+    }
+
+    printk( KERN_DEBUG "faye_subDevice register success\n" );
+
+    printk( "/**** faye_device_init end ***************************************/\n" );
+    return 0;
 }
 
 static void __exit faye_device_exit( void ){
-	printk( "/**** faye_device_exit() start ***************************************/\n" );
+    printk( "/**** faye_device_exit() start ***************************************/\n" );
 
-	device_unregister( &faye_device );
-	printk( "faye_device exit success!\n" );
-	
-	device_unregister( &faye_device_class );
-	printk( "faye_device_class exit success!\n" );
-	
-	printk( "/**** faye_device_exit() end ***************************************/\n" );
+    device_unregister( &faye_device );
+    printk( "faye_device exit success!\n" );
+    
+    device_unregister( &faye_device_class );
+    printk( "faye_device_class exit success!\n" );
+    
+    printk( "/**** faye_device_exit() end ***************************************/\n" );
 }
 
 module_init( faye_device_init );
